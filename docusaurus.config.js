39ffdd499b1df0535242504185464010d463e53b
mohamilin <amilinmoh99@gmail.com>
2022-03-08 21:40:59 +0700

    fix pretiier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,38 +1,38 @@
 // @ts-nocheck
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Promeet Labs",
-  tagline: "Happy Coding !!!",
-  url: "https://promeet-id.github.io",
-  baseUrl: "/labs/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "Promeet-id", // Usually your GitHub org/user name.
-  projectName: "labs", // Usually your repo name.
+  title: 'Promeet Labs',
+  tagline: 'Happy Coding !!!',
+  url: 'https://promeet-id.github.io',
+  baseUrl: '/labs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Promeet-id', // Usually your GitHub org/user name.
+  projectName: 'labs', // Usually your repo name.
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: "https://github.com/Promeet-id/labs",
+          editUrl: 'https://github.com/Promeet-id',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: "https://github.com/Promeet-id/labs",
+          editUrl: 'https://github.com/Promeet-id',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -42,35 +42,35 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Promeet - Labs",
+        title: 'Promeet - Labs',
         logo: {
-          alt: "My Site Logo",
-          src: "img/promeetid.jpg",
+          alt: 'My Site Logo',
+          src: 'img/promeetid.jpg',
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Tutorial",
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: "https://github.com/Promeet-id/labs",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/Promeet-id',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
+                label: 'Tutorial',
+                to: '/docs/intro',
               },
             ],
           },
@@ -92,15 +92,15 @@ const config = {
           //   ],
           // },
           {
-            title: "More",
+            title: 'More',
             items: [
               {
-                label: "Blog",
-                to: "/blog",
+                label: 'Blog',
+                to: '/blog',
               },
               {
-                label: "GitHub",
-                href: "https://github.com/Promeet-id/labs",
+                label: 'GitHub',
+                href: 'https://github.com/Promeet-id',
               },
             ],
           },
